395dc5bd888a3808a597a3bf797b11a5134f128f
duiliod <duiliodeangeli37@gmail.com>
2022-06-22 15:40:48 -0300

    dataloader in ae_training for MRI images

--- src/optim/ae_trainer.py
+++ src/optim/ae_trainer.py
@@ -10,6 +10,36 @@ import torch.optim as optim
 import numpy as np
 
 
+import nibabel as ni
+import os, glob
+import csv
+from tqdm import tqdm
+import shutil
+import random
+def load_mri_images(path, batch_size):
+    filenames = [i for i in os.listdir(path) if i.endswith(".nii")] #and i.startswith("norm_023_S_0030")
+    random.shuffle(filenames, random.random)
+    n = 0
+    while n < len(filenames):
+        batch_image = []
+        for i in range(n, n + batch_size):
+            if i >= len(filenames):
+                ##n = i
+                break
+            #print(filenames[i])
+            image = ni.load(os.path.join(path, filenames[i]))
+            image = np.array(image.dataobj)
+            # image = np.pad(image, ((1,0), (1,0), (1, 0)), "constant", constant_values=0)
+            image = torch.Tensor(image)
+            image = torch.reshape(image, (1,1, 48, 48, 48))
+            #image = (image - image.min()) / (image.max() - image.min())
+            image = image / 255.
+            batch_image.append(image)
+        n += batch_size
+        batch_image = torch.cat(batch_image)
+        yield batch_image
+
+
 class AETrainer(BaseTrainer):
 
     def __init__(self, optimizer_name: str = 'adam', lr: float = 0.001, n_epochs: int = 150, lr_milestones: tuple = (),
@@ -46,8 +76,13 @@ class AETrainer(BaseTrainer):
             loss_epoch = 0.0
             n_batches = 0
             epoch_start_time = time.time()
+
+            #For hippocampal MRI
+            # for batch_images in tqdm(load_mri_images("/home/duilio/Downloads/seg/cropped_data", 8)):
+            #     print((batch_images.shape))  #torch.Size([8, 1, 48, 48, 48])
             for data in train_loader:
-                inputs, _, _ = data
+                inputs, _, _ = data #all labels are 0 cuz are the normal class
+                # ae training data shape torch.Size([200, 1, 28, 28]) batch size 200
                 inputs = inputs.to(self.device)
 
                 # Zero the network parameter gradients
@@ -85,36 +120,37 @@ class AETrainer(BaseTrainer):
 
         # Testing
         logger.info('Testing autoencoder...')
-        loss_epoch = 0.0
-        n_batches = 0
-        start_time = time.time()
-        idx_label_score = []
-        ae_net.eval()
-        with torch.no_grad():
-            for data in test_loader:
-                inputs, labels, idx = data
-                inputs = inputs.to(self.device)
-                outputs = ae_net(inputs)
-                scores = torch.sum((outputs - inputs) ** 2, dim=tuple(range(1, outputs.dim())))
-                loss = torch.mean(scores)
+        # loss_epoch = 0.0
+        # n_batches = 0
+        # start_time = time.time()
+        # idx_label_score = []
+        # ae_net.eval()
+        # with torch.no_grad():
+        #     for data in test_loader:
+        #         inputs, labels, idx = data #labels contain normal and anomalies
+        #         # shape torch.Size([200, 1, 28, 28]) torch.Size([200]) torch.Size([200]
+        #         inputs = inputs.to(self.device)
+        #         outputs = ae_net(inputs)
+        #         scores = torch.sum((outputs - inputs) ** 2, dim=tuple(range(1, outputs.dim())))
+        #         loss = torch.mean(scores)
 
-                # Save triple of (idx, label, score) in a list
-                idx_label_score += list(zip(idx.cpu().data.numpy().tolist(),
-                                            labels.cpu().data.numpy().tolist(),
-                                            scores.cpu().data.numpy().tolist()))
+        #         # Save triple of (idx, label, score) in a list
+        #         idx_label_score += list(zip(idx.cpu().data.numpy().tolist(),
+        #                                     labels.cpu().data.numpy().tolist(),
+        #                                     scores.cpu().data.numpy().tolist()))
 
-                loss_epoch += loss.item()
-                n_batches += 1
+        #         loss_epoch += loss.item()
+        #         n_batches += 1
 
-        logger.info('Test set Loss: {:.8f}'.format(loss_epoch / n_batches))
+        # logger.info('Test set Loss: {:.8f}'.format(loss_epoch / n_batches))
 
-        _, labels, scores = zip(*idx_label_score)
-        labels = np.array(labels)
-        scores = np.array(scores)
+        # _, labels, scores = zip(*idx_label_score)
+        # labels = np.array(labels)
+        # scores = np.array(scores)
 
-        auc = roc_auc_score(labels, scores)
-        logger.info('Test set AUC: {:.2f}%'.format(100. * auc))
+        # auc = roc_auc_score(labels, scores)
+        # logger.info('Test set AUC: {:.2f}%'.format(100. * auc))
 
-        test_time = time.time() - start_time
-        logger.info('Autoencoder testing time: %.3f' % test_time)
+        # test_time = time.time() - start_time
+        # logger.info('Autoencoder testing time: %.3f' % test_time)
         logger.info('Finished testing autoencoder.')
